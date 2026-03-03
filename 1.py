237842759b0cf01c4c7398984a92eaade7f88bf8
Quad <86720835+quad58@users.noreply.github.com>
2026-03-03 11:44:12 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -5,5 +5,5 @@ s = "37 367 125 56 34 247 126".split()
 v = "AD DE EG GC CF FA FB AB BE".split()
 print("1 2 3 4 5 6 7")
 for p in permutations("ABCDEFG"):
-    if all(str(p.index(a) + 1) in s[p.index(b)] for a, b in v:
+    if all(str(p.index(a) + 1) in s[p.index(b)] for a, b in v):
         print(*p)
