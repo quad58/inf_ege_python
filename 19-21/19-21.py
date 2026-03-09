e81ea6fd2c79f55eec0e44365179909ccc2d4701
Quad <86720835+quad58@users.noreply.github.com>
2026-03-09 21:44:41 +0300

    19-21

--- 19-21/19-21.py
+++ 19-21/19-21.py
@@ -73,11 +73,31 @@
 # print(21, [s for s in range(5, 151) if (not f(130, s, 1)) and(not f(130, s, 3)) and f(130, s, 5)]) # 162020880
 
 # 12737
-def f(a, b, m):
-    if a * b > 384: return m%2 == 0
+# def f(a, b, m):
+#     if a * b > 384: return m%2 == 0
+#     if m == 0: return 0
+#     h = [f(a+5, b, m-1), f(a, b+5, m-1), f(a*2, b, m-1), f(a, b*2, m-1)]
+#     return any(h) if (m-1)%2 == 0 else all(h)
+# print(19, [s for s in range(1, 55) if f(8, s, 2)])
+# print(20, [s for s in range(1, 55) if (not f(8, s, 1)) and f(8, s, 3)])
+# print(21, [s for s in range(1, 55) if (not f(8, s, 2)) and f(8, s, 4)])
+
+# 27631
+# def f(a, b, m):
+#     if a + b >= 211: return m % 2 == 0
+#     if m == 0: return 0
+#     h = [f(a+1, b, m-1), f(a, b+1, m-1), f(a*2, b, m-1), f(a, b*2, m-1)]
+#     return any(h) if (m-1)%2 == 0 else all(h)
+# print(19, [s for s in range(1, 194) if f(17, s, 2)]) # any
+# print(19, [s for s in range(1, 194) if (not f(17, s, 1)) and f(17, s, 3)])
+# print(19, [s for s in range(1, 194) if (not f(17, s, 2)) and f(17, s, 4)])
+
+# 23759
+def f(a, m):
+    if a <= 30: return m % 2 == 0
     if m == 0: return 0
-    h = [f(a+5, b, m-1), f(a, b+5, m-1), f(a*2, b, m-1), f(a, b*2, m-1)]
+    h = [f(a-3, m-1), f(a-5, m-1), f(a//4, m-1)]
     return any(h) if (m-1)%2 == 0 else all(h)
-print(19, [s for s in range(1, 55) if f(8, s, 2)])
-print(20, [s for s in range(1, 55) if (not f(8, s, 1)) and f(8, s, 3)])
-print(21, [s for s in range(1, 55) if (not f(8, s, 2)) and f(8, s, 4)])
+print(19, [s for s in range(31, 1000) if f(s, 2)]) # all
+print(19, [s for s in range(31, 1000) if (not f(s, 1)) and f(s, 3)])
+print(19, [s for s in range(31, 1000) if (not f(s, 2)) and f(s, 4)])
