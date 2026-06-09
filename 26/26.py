3f02b80b9f39e27bcaff8bfbfde949088fcc6f7b
Quad <86720835+quad58@users.noreply.github.com>
2026-06-09 15:26:30 +0300

    26

--- 26/26.py
+++ 26/26.py
@@ -132,17 +132,120 @@
 # print([x for x in a if x[0] >= b[-1][1]])
 
 #k 6791
-f = open("26/26-129.txt")
-N = int(f.readline())
+# f = open("26/26-129.txt")
+# N = int(f.readline())
+# a = []
+# k = 0
+# for s in f:
+#     k += 1
+#     sh, ok = [int(x) for x in s.split()]
+#     if sh < ok:
+#         a.append([sh, "sh", k])
+#     else:
+#         a.append([ok, "ok", k])
+# a.sort()
+# print(a[-1])
+# print(len([x for x in a if x[1] == "sh"])-1)
+
+#k 7506
+# f = open("26/26-151.txt")
+# N, S = [int(x) for x in f.readline().split()]
+# a = []
+# for s in f:
+#     ID, b1, b2, b3, so = [int(x) for x in s.split()]
+#     a.append([ID, b1 + b2 + b3, so])
+
+# a.sort(key = lambda x: (x[1], x[2], -x[0]), reverse=1)
+# print(a[S - 1])
+# print(a[S - 50: S + 50])
+# print(len([x for x in a if x[1] == 154]))
+
+#k 7620
+# f = open("26/26-154.txt")
+# N = int(f.readline())
+# a = []
+# b = []
+# for s in f:
+#     ID, b1, b2, b3, b4 = [int(x) for x in s.split()]
+#     if 2 not in [b1, b2, b3, b4]:
+#         a.append([ID, sum([b1, b2, b3, b4]) / 4])
+#     else:
+#         b.append([ID, len([x for x in [b1, b2, b3, b4] if x == 2])])
+# a.sort(key = lambda x: (x[1], -x[0]), reverse=1)
+# b.sort(key = lambda x: (x[1], x[0]))
+# print(a[N // 4 - 1])
+# print([x for x in b if x[1] > 2][0])
+
+#k 4933
+# f = open("26/26-75.txt")
+# N = int(f.readline())
+# timeline = [0] * 1000000
+# for s in f:
+#     start, end = [int(x) for x in s.split()]
+#     for i in range(start, end):
+#         timeline[i] += 1
+# print(max(timeline))
+# print(len([x for x in timeline if x != 0]))
+
+#k 7109
+# f = open("26/26-140.txt")
+# N = int(f.readline())
+# timeline = [" "] * 44640
+# for s in f:
+#     start, end = [int(x) for x in s.split()]
+#     for i in range(start, end):
+#         timeline[i] = "1"
+# print(timeline.count("1"), max([len(c) for c in ''.join(timeline).split()]))
+
+#k 6316
+# f = open("26/26-111.txt")
+# K = int(f.readline())
+# N = int(f.readline())
+# a = []
+# for s in f:
+#     start, end = [int(x) for x in s.split()]
+#     a.append([start, end])
+# a.sort()
+# kamera = [0] * K
+# r = []
+# for start, end in a:
+#     for i in range(K):
+#         if kamera[i] < start:
+#             kamera[i] = end
+#             r.append(i + 1)
+#             break
+# print(len(r), r[-1])
+
+#k 6406
+f = open("26/26-119.txt")
+N, L, M = [int(x) for x in f.readline().split()]
 a = []
-k = 0
 for s in f:
-    k += 1
-    sh, ok = [int(x) for x in s.split()]
-    if sh < ok:
-        a.append([sh, "sh", k])
-    else:
-        a.append([ok, "ok", k])
+    st, d, t = s.split()
+    st, d = int(st), int(d)
+    a.append([st, d, t])
 a.sort()
-print(a[-1])
-print(len([x for x in a if x[1] == "sh"])-1)
+l = [0] * L
+m = [0] * M
+kl = 0
+km = 0
+for st, d, t in a:
+    if t == "A":
+        for i in range(L):
+            if l[i] <= st:
+                l[i] = st+d
+                kl += 1
+                break
+        else:
+            for i in range(M):
+                if m[i] <= st:
+                    m[i] = st + d
+                    kl += 1
+                    break
+    if t == "B":
+        for i in range(M):
+            if m[i] <= st:
+                m[i] = st+d
+                km += 1
+                break
+print(km, N - kl - km)
