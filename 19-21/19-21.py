18ad93cd1a2e8c8f3180c9a78f9e15d4e713ae1e
Quad <86720835+quad58@users.noreply.github.com>
2026-06-17 18:25:58 +0300

    19

--- 19-21/19-21.py
+++ 19-21/19-21.py
@@ -59,7 +59,7 @@
 #     return any(h) if (m-1)%2 == 0 else all(h)
 # print(19, [s for s in range(1, 91) if f(6, s, 2)])
 # print(20, [s for s in range(1, 91) if (not f(6, s, 1)) and f(6, s, 3)])
-# print(20, [s for s in range(1, 91) if (not f(6, s, 2)) and f(6, s, 4)])
+# print(21, [s for s in range(1, 91) if (not f(6, s, 2)) and f(6, s, 4)])
 
 # 23329
 # from math import *
@@ -89,15 +89,27 @@
 #     h = [f(a+1, b, m-1), f(a, b+1, m-1), f(a*2, b, m-1), f(a, b*2, m-1)]
 #     return any(h) if (m-1)%2 == 0 else all(h)
 # print(19, [s for s in range(1, 194) if f(17, s, 2)]) # any
-# print(19, [s for s in range(1, 194) if (not f(17, s, 1)) and f(17, s, 3)])
-# print(19, [s for s in range(1, 194) if (not f(17, s, 2)) and f(17, s, 4)])
+# print(20, [s for s in range(1, 194) if (not f(17, s, 1)) and f(17, s, 3)])
+# print(21, [s for s in range(1, 194) if (not f(17, s, 2)) and f(17, s, 4)])
 
 # 23759
+# def f(a, m):
+#     if a <= 30: return m % 2 == 0
+#     if m == 0: return 0
+#     h = [f(a-3, m-1), f(a-5, m-1), f(a//4, m-1)]
+#     return any(h) if (m-1)%2 == 0 else all(h)
+# print(19, [s for s in range(31, 1000) if f(s, 2)]) # all
+# print(20, [s for s in range(31, 1000) if (not f(s, 1)) and f(s, 3)])
+# print(21, [s for s in range(31, 1000) if (not f(s, 2)) and f(s, 4)])
+
+# 29974
+from math import *
+
 def f(a, m):
-    if a <= 30: return m % 2 == 0
+    if a <= 15: return m % 2 == 0
     if m == 0: return 0
-    h = [f(a-3, m-1), f(a-5, m-1), f(a//4, m-1)]
+    h = [f(a-3, m-1), f(a-7, m-1), f(floor(a/4), m-1)]
     return any(h) if (m-1)%2 == 0 else all(h)
-print(19, [s for s in range(31, 1000) if f(s, 2)]) # all
-print(19, [s for s in range(31, 1000) if (not f(s, 1)) and f(s, 3)])
-print(19, [s for s in range(31, 1000) if (not f(s, 2)) and f(s, 4)])
+print(19, [s for s in range(16, 100) if f(s, 2)]) # all
+print(20, [s for s in range(16, 100) if (not f(s, 1)) and f(s, 3)])
+print(21, [s for s in range(16, 100) if (not f(s, 2)) and f(s, 4)])
